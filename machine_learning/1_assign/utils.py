d8d656104cfcdf3c899263bee8006660a0795988
mccarvik <mccarviks@gmail.com>
2024-02-03 20:35:29 -0500

    nn

--- machine_learning/1_assign/utils.py
+++ machine_learning/1_assign/utils.py
@@ -5,30 +5,31 @@ This file contains utility functions for the machine learning assignment.
 import matplotlib.pyplot as plt
 
 
-def val_curve(values, output, model=""):
+def val_curve(values, output, model="", x_axis=""):
     """
-    Plot a learning curve
-    """
-
-    if model == "knn":
-        x_axis = "value for K"
-
-    plt.plot(values, output)
-    plt.title("Learning curve - {}".format(model))
-    plt.xlabel(x_axis)
-    plt.ylabel("Accuracy")
-    plt.savefig("pngs/learning_curve_{}.png".format(model), dpi=300)
-    plt.close()
-
-
-def learn_curve(values, output, model="", x_axis="percentage of training data"):
-    """
-    Plot a Learning curve
+    Plot a Validatuon curve
     """
 
     plt.plot(values, output)
-    plt.title("Validation curve - {}". format(model))
+    plt.title("Validation curve - {}".format(model))
     plt.xlabel(x_axis)
     plt.ylabel("Accuracy")
     plt.savefig("pngs/validation_curve_{}.png".format(model), dpi=300)
+    plt.close()
+
+
+def learn_curve(values, train_output, test_output, model="", x_axis="percentage of training data"):
+    """
+    Plot a Learning curve
+    """
+    plt.figure(figsize=(10, 6))
+    # Plot the mean training scores
+    plt.plot(values, train_output, label="Training score", color="darkorange", marker='o')
+    # Plot the mean test scores
+    plt.plot(values, test_output, label="Test Score", color="navy", marker='o')
+    plt.legend(loc="best")
+    plt.title("Learning curve - {}". format(model))
+    plt.xlabel(x_axis)
+    plt.ylabel("Accuracy")
+    plt.savefig("pngs/learning_curve_{}.png".format(model), dpi=300)
     plt.close()
